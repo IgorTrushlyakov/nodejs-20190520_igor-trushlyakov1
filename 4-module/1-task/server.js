4e369f3e1b3997640f0a6e17ada239ac7c1d15e6
Igor Trushlyakov <igortr89@gmail.com>
2019-06-04 23:59:38 +0300

    add solution 4-module 1-task

--- 4-module/1-task/server.js
+++ 4-module/1-task/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -11,6 +12,23 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'GET':
+
+      if (pathname.match('[\\/]')) {
+        res.statusCode = 400;
+        res.end('path is nested');
+      }
+
+      fs.createReadStream(filepath)
+        .on('error', (err) => {
+          if (err.code === 'ENOENT') {
+            res.statusCode = 404;
+            res.end('not found');
+          } else {
+            res.statusCode = 500;
+            res.end('server error')
+          }
+      })
+        .pipe(res);
 
       break;
 
