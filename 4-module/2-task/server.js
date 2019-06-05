d0ca1c4c044adbaf5e9b8a0db68056adc6d49b5b
Igor Trushlyakov <igortr89@gmail.com>
2019-06-05 13:27:55 +0300

    add solution for 4-module 2-task

--- 4-module/2-task/server.js
+++ 4-module/2-task/server.js
@@ -1,6 +1,8 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+const LimitSizeStream = require('./LimitSizeStream');
 
 const server = new http.Server();
 
@@ -11,6 +13,34 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'POST':
+      if (pathname.match('[\\/]')) {
+        res.statusCode = 400;
+        res.end('path is nested');
+      }
+
+      const limitedStream = new LimitSizeStream({limit: 1048576});
+
+      req
+        .pipe(limitedStream)
+        .on('error', (err) => {
+          err.statusCode = 413;
+          fs.unlink(filepath, () => {});
+          res.end();
+        })
+        .pipe(fs.createWriteStream(filepath, {flags: 'wx'}))
+        .on('error', err => {
+          if (err.code === 'EEXIST') {
+            res.statusCode = 409;
+            res.end('already exist');
+          } else {
+            res.statusCode = 500;
+            res.end('server error')
+          }
+        })
+        .on('close', () => {
+          res.statusCode = 201;
+          res.end('ok')
+        });
 
       break;
 
@@ -18,6 +48,14 @@ server.on('request', (req, res) => {
       res.statusCode = 501;
       res.end('Not implemented');
   }
+
+  req.on('close', () => {
+    if (res.finished) {
+      return;
+    }
+    fs.unlink(filepath, () => {});
+  });
+
 });
 
 module.exports = server;
